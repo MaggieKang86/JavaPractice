f9c1927f2ab945d8bcef5a4b29f2ad06d0c72698
Maggie Kang <maggiekang86@gmail.com>
2022-12-16 16:31:08 +0800

    新增Ch05_1111216

---
+++ basic/Ch5_1111216/src/ch5_1111216/Ch5_1111216_1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ch5_1111216;
+
+/**
+ *
+ * @author user
+ */
+public class Ch5_1111216_1 {
+
+    public static void main(String[] args) {
+        System.out.println("1 Play");
+        System.out.println("2 Stop");
+        System.out.println("3 Start");
+        int action = 4;
+        //switch case
+        if (action == 1){
+            System.out.println("Play");
+        } else if (action == 2) {
+            System.out.println("Stop"); 
+        } else {
+            System.out.println("Start");
+        }
+                
+    }
+    
+}
