498f8099b4ebc6c7fbd309638ce5d2176c922c66
Maggie Kang <maggiekang86@gmail.com>
2022-12-21 11:48:26 +0800

    結束Ch5_4及新增Ch5_5

---
+++ basic/Ch05_1111220/src/Ch5_5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Ch5_5 {
+    
+    public static void main(String[] args) {
+       for (int i = 1; i <=5; i++){
+           for(int k = i; k <= 5; k++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+}
--- basic/Ch05_1111220/src/ch5_1111220/Ch5_4.java
+++ basic/Ch05_1111220/src/ch5_1111220/Ch5_4.java
@@ -9,8 +9,10 @@ public class Ch5_4 {
     public static void main(String[] args) {
         //巢狀迴圈
         for (int i = 1; i <= 9; i++){
-            
-            
+            for ( int x = 1; x <= 9; x++){
+                System.out.printf("%d * %d = %02d ",i,x,i*x);
+            }
+            System.out.println();
         }
     }
 }
