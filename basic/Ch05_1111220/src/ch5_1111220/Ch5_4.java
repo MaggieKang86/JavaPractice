ad80f0e2d73164697de4f19d60fb3e1b9b756130
林俞均 <anita.lin@promeritage.com.tw>
2022-12-21 13:49:03 +0800

    Reformat code

--- basic/Ch05_1111220/src/ch5_1111220/Ch5_4.java
+++ basic/Ch05_1111220/src/ch5_1111220/Ch5_4.java
@@ -1,18 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ch5_1111220;
 
 public class Ch5_4 {
-    
+
     public static void main(String[] args) {
         //巢狀迴圈
-        for (int i = 1; i <= 9; i++){
-            for ( int x = 1; x <= 9; x++){
-                System.out.printf("%d * %d = %02d ",i,x,i*x);
+        for (int i = 1; i <= 9; i++) {
+            for (int x = 1; x <= 9; x++) {
+                System.out.printf("%d * %d = %02d ", i, x, i * x);
             }
             System.out.println();
         }
     }
+
 }
--- basic/Ch05_1111220/src/ch5_1111220/Ch5_5.java
+++ basic/Ch05_1111220/src/ch5_1111220/Ch5_5.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package ch5_1111220;
 
-/**
- *
- * @author user
- */
 public class Ch5_5 {
-    
+
     public static void main(String[] args) {
-       for (int i = 1; i <=5; i++){
-           for(int k = i; k <= 5; k++){
-               System.out.print("*");
-           }
-           System.out.println();
-       }
+        for (int i = 1; i <= 5; i++) {
+            for (int k = i; k <= 5; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+
 }
