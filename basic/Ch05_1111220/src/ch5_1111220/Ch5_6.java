dfe25b724ec02da55f8f9eaa72f84d0ce3530e70
Maggie Kang <maggiekang86@gmail.com>
2022-12-21 16:49:59 +0800

    我也不知道我幹嘛了 忘記了

---
+++ basic/Ch05_1111220/src/ch5_1111220/Ch5_6.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Ch5_6 {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 5; i++) {
+        System.out.println("Start:"+i);
+        if (i == 3){
+        break;
+        }
+            for(int = k; k <= 3; k++) {
+            System.out.printf("Body %d_%d",k,i);
+            
+            
+        }      
+            System.out.println();
+            System.out.println("End:"+i);
+            
+            
+        }
+        
+        
+    }
+}
