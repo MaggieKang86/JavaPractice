8d2a5232dcb806a91eebdf53685a4f1d0463e53a
林俞均 <anita.lin@promeritage.com.tw>
2022-12-23 09:30:14 +0800

    調整資料夾結構

---
+++ basic/Ch01/1111206/src/Ch1_1.java
@@ -0,0 +1,10 @@
+/**
+ * @author maggie
+ */
+public class Ch1_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Ken");
+    }
+
+}
---
+++ basic/Ch03/1111208/src/Ch3_5.java
@@ -0,0 +1,23 @@
+/**
+ * @author maggie
+ */
+public class Ch3_5 {
+
+    public static void main(String[] args) {
+        int a1 = 19, b2 = 5;
+        System.out.printf("%d + %d = %d%n", a1, b2, a1 + b2);
+        System.out.printf("%d - %d = %d%n", a1, b2, a1 - b2);
+        System.out.printf("%d * %d = %d%n", a1, b2, a1 * b2);
+        //求商數
+        System.out.printf("%d / %d = %d%n", a1, b2, a1 / b2);
+        //求餘數
+        System.out.printf("%d %% %d = %d%n", a1, b2, a1 % b2);
+
+        //算式當中其中一組是浮點數類型的 計算結果就是浮點數
+        float maxHp = 100;
+        int hp = 90;
+        float bar = hp / maxHp;
+        System.out.println(bar * 100);
+    }
+
+}
