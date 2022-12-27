0d260d69e472ade3d56b62d5964e65454ddc2b84
Maggie Kang <maggiekang86@gmail.com>
2022-12-27 17:22:10 +0800

    修正

--- OCP/Ch01_02_ClassAndObjects/src/MainClass.java
+++ OCP/Ch01_02_ClassAndObjects/src/MainClass.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MainClass {
+public class MainClass {
+
+    public static void main(String[] args) {
+        //物件建立語法：類別型別 參考名稱 = new 建構子();
+        Account user1 = new Account();
+        //型別              Account.java裡建構子的Account
+
+        //物件操作
+        //語法：參考名稱.物件成員
+        user1.name = "Lisa";
+        user1.balance = 500;
+        System.out.println("user1");
+        user1.display();
+        System.out.println("---------------------");
+    }
+
+
+
 }
--- basic/Ch01/1111206/src/Ch1_1.java
+++ basic/Ch01/1111206/src/Ch1_1.java
@@ -4,7 +4,7 @@
 public class Ch1_1 {
 
     public static void main(String[] args) {
-        System.out.println("Ken");
+    System.out.println("Ken");
     }
 
 }
