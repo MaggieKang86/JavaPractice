1b183461e0737d42861a5484edbd234b39c6f9ae
Maggie Kang <maggiekang86@gmail.com>
2022-12-27 09:01:49 +0800

    調整資料夾

---
+++ basic/Ch03/1111208/src/Ch3_2.java
@@ -0,0 +1,19 @@
+/**
+ * @author maggie
+ */
+public class Ch3_2 {
+
+    public static void main(String[] args) {
+        //布林值表示true or false
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+
+        //常數：不可變動的數值
+        final double PI = 3.1415967;
+        double angle = Math.sin(PI / 2) * 180 / PI;
+        System.out.println(angle);
+    }
+
+}
---
+++ basic/Ch04/1111212/src/Ch4_1.java
@@ -0,0 +1,21 @@
+/**
+ * @author maggie
+ */
+public class Ch4_1 {
+
+    public static void main(String[] args) {
+        // 比較運算值(true amd false)
+        int a = 10, b = 5;
+        System.out.printf("%d > %d = %b%n", a, b, a > b);
+        System.out.printf("%d < %d = %b%n", a, b, a < b);
+        System.out.printf("%d >= %d = %b%n", a, b, a >= b);
+        System.out.printf("%d <= %d = %b%n", a, b, a <= b);
+
+        // 是否相等是"=="
+        System.out.printf("%d == %d = %b%n", a, b, a == b);
+
+        // 是否不相等是"!="
+        System.out.printf("%d != %d = %b%n", a, b, a != b);
+    }
+
+}
