86345396e0765002785d5550554ebbd18e204243
Maggie Kang <maggiekang86@gmail.com>
2022-12-29 16:42:22 +0800

    OCP02上String

--- OCP/Ch01_02_ClassAndObjects/src/com/sample/MainClass.java
+++ OCP/Ch01_02_ClassAndObjects/src/com/sample/MainClass.java
@@ -11,9 +11,34 @@ public class MainClass {
         //語法：參考名稱.物件成員
         user1.name = "Lisa";
         user1.balance = 500;
-        System.out.println("user1");
+        System.out.println("user1:");
         user1.display();
         System.out.println("---------------------");
+
+        Account user2 = new Account();
+        user2.name = "Maggie";
+        user2.balance = 2000000;
+        System.out.println("user2:");
+        user2.display();
+        System.out.println("---------------------");
+
+        Account user3 = user2;
+        System.out.println("user3:");
+        user3.balance = 5000;
+        user3.display();
+        System.out.println("user2:");
+        user2.display();
+        System.out.println("---------------------");
+
+        user1 = user3;
+        System.out.println("user1:");
+        user1.display();
+        System.out.println("---------------------");
+
+        //-----------------------------------------
+        //null 空：物件型別的預設值
+        user2 = null; //代表user2不指向任何物件
+        //user2.balance = 2000; //NullPointerException
     }
 
 }
