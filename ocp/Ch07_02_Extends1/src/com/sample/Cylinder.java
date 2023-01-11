1206bab0a58e75532c442ec7510492246e37df2f
Maggie Kang <maggiekang86@gmail.com>
2023-01-11 09:39:10 +0800

    結束ocp 07

--- ocp/Ch07_02_Extends1/src/com/sample/Cylinder.java
+++ ocp/Ch07_02_Extends1/src/com/sample/Cylinder.java
@@ -2,4 +2,28 @@ package com.sample;
 
 public class Cylinder extends Circle {
 
+    private  double height;
+
+    public Cylinder() {
+        super();  // 呼叫父類別的建構子，完成父類別初始化，目前可省略(因為父類別建構子不用參數)
+                  // 若要撰寫，必須寫在建構子的第一行
+        System.out.println("Cylinder()...");
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    // 複寫父類別方法：當父類別提供的方法不適合子類別運行，子類別可以改寫父類別的方法
+    @Override
+    /*
+    public double getArea() { return PI * getRadius() * getRadius() * 2 + 2 * PI * getRadius() * height;  }
+    */
+    public double getArea() {
+        return super.getArea() * 2 + getCircumference() * height;
+    }
 }
--- ocp/Ch07_02_Extends1/src/com/sample/MainClass.java
+++ ocp/Ch07_02_Extends1/src/com/sample/MainClass.java
@@ -7,9 +7,12 @@ public class MainClass {
         Circle cir = new Circle();
         cir.setRadius(10);
         System.out.printf("cir[r:%.2f, a:%.2f, c:%.2f]%n", cir.getRadius(), cir.getArea(), cir.getCircumference());
+        System.out.println("-------------");
 
         Cylinder cyl = new Cylinder();
-
+        cyl.setRadius(10);
+        cyl.setHeight(10);
+        System.out.printf("cyl[r:%.2f, h:%.2f, a:%.2f]%n", cyl.getRadius(), cyl.getHeight(), cyl.getArea());
     }
 
 
