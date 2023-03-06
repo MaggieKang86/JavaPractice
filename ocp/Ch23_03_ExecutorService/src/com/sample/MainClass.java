c8cc76071348773c193b1d144748c5f06b5b0249
Maggie Kang <maggiekang86@gmail.com>
2023-03-06 16:39:33 +0800

    ocp23 1:04:51

---
+++ ocp/Ch23_03_ExecutorService/src/com/sample/MainClass.java
@@ -0,0 +1,32 @@
+package com.sample;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        ExecutorService es = Executors.newFixedThreadPool(4);
+
+        for(int i = 1; i <= 15; i++) {
+            int NUM = i;
+            es.execute(new Runnable() {
+                @Override
+                public void run() {
+                    int time = new Random().nextInt(5000);
+                    System.out.println(Thread.currentThread() + "\t" + NUM + "\t" + time);
+                    try {
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        System.out.println(e);
+                    }
+                }
+            });
+        }
+        es.shutdown();
+
+    }
+
+}
