ea7f6901e3a801187e2d27a36fbedf23a2533cf8
maggiekang86 <maggiekang86@gmail.com>
2023-06-05 18:43:01 +0800

    補上傳

--- javaweb/servlet02/src/com/bjpowernode/javaweb/sevlet/GenericServlet.java
+++ javaweb/servlet02/src/com/bjpowernode/javaweb/sevlet/GenericServlet.java
@@ -11,14 +11,31 @@ GenericServlet 就是一個適配器
  */
 public abstract class GenericServlet implements Servlet {
 
+    // 成員變量
+    private ServletConfig config;
+
+    /*
+    init 方法中的 ServletConfig 對象是 Tomcat 創建的
+    這個 ServletConfig 對象目前在 init 方法的參數上屬於區域變數
+    那 ServletConfig 對象肯定以後會要在 service 方法中使用，不過要怎麼樣才能保證 ServletConfig 在 service 中可以被使用呢
+     */
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
+    public final void init(ServletConfig servletConfig) throws ServletException {
+        // System.out.println("servletConfig 對象，Tomcat 創建好的：" + config);
+        this.config = config;
+
+        // 調用 init() 方法
+        this.init();
+    }
+
+    // 供子類 override 用的
+    public void init() {
 
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return config;
     }
 
     /*
--- javaweb/servlet02/src/com/bjpowernode/javaweb/sevlet/LoginService.java
+++ javaweb/servlet02/src/com/bjpowernode/javaweb/sevlet/LoginService.java
@@ -1,5 +1,6 @@
 package com.bjpowernode.javaweb.sevlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -7,9 +8,30 @@ import java.io.IOException;
 
 public class LoginService extends GenericServlet{
 
+    // Q:是否有需要在 LoginServlet class 中覆寫 init 方法？
+    // A:有可能的，所以覆寫 init method
+    /*
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("我重寫了自己的規則...");
+    }
+    */
+
+    // 父類將原始的 init 方法 final，使子類無法 override
+    // 但此時我仍希望 override，怎辦
+    @Override
+    public void init() {
+        System.out.println("LoginServlet's init() method execute!");
+    }
+
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
         System.out.println("正在處理用戶請求，請稍候...");
+        // 如果想在 LoginServlet 子類中使用 ServletConfig 怎麼辦？
+        // 因為繼承了 GenericServlet，所以可使用其中的 getServletConfig 方法
+        ServletConfig config = this.getServletConfig();
+        System.out.println("service 方法中是否可以使用 ServletConfig 對象？：" + config);
+
     }
 
 }
