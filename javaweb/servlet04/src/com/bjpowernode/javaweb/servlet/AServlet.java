c24f1108f723d8bdeef1c9ffde9c71f5b7b158c7
maggiekang86 <maggiekang86@gmail.com>
2023-06-07 22:06:31 +0800

    上傳bilibili 15+16/76

--- javaweb/servlet04/src/com/bjpowernode/javaweb/servlet/AServlet.java
+++ javaweb/servlet04/src/com/bjpowernode/javaweb/servlet/AServlet.java
@@ -3,6 +3,7 @@ package com.bjpowernode.javaweb.servlet;
 import javax.servlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class AServlet extends GenericServlet {
     @Override
@@ -11,6 +12,35 @@ public class AServlet extends GenericServlet {
         PrintWriter out = response.getWriter();
         // 獲取 ServletContext 對象
         ServletContext application = this.getServletContext();
-        out.print("ServletContext對象是: " + application);
+        out.print("ServletContext對象是: " + application + "<br>");
+
+        // 獲取上下文的初始化參數
+        Enumeration<String> initParameterNames = application.getInitParameterNames();  // 返回初始化參數的名字
+        while(initParameterNames.hasMoreElements()){
+            String name = initParameterNames.nextElement();
+            String value = application.getInitParameter(name);
+            out.print(name + "=" + value + "<br>");
+        }
+
+        // 獲取 context path (動態獲取應用上下文的源頭)
+        String contextPath = application.getContextPath();
+        out.print(contextPath + "<br>");
+
+        // 動態取得文件的絕對路徑
+        String realPath = application.getRealPath("/index.html"); // () 中的 "/" 可寫可不寫
+        out.print(realPath + "<br>");
+
+        String realPath1 = application.getRealPath("/common/common.html");
+        out.print(realPath + "<br>");
+
+        // 準備數據
+        User user = new User("jack", "123");
+        // 向 ServletContext 應用域當中儲存數據
+        application.setAttribute("userObj", user);
+        // 在 AServlet 取出
+        // Object userObj = application.getAttribute("userObj");
+        // 輸出到瀏覽器
+        // out.print(userObj + "<br>");
+
     }
 }
--- javaweb/servlet04/src/com/bjpowernode/javaweb/servlet/BServlet.java
+++ javaweb/servlet04/src/com/bjpowernode/javaweb/servlet/BServlet.java
@@ -12,5 +12,17 @@ public class BServlet extends GenericServlet {
         // 獲取 ServletContext 對象
         ServletContext application = this.getServletContext();
         out.print("ServletContext對象是: " + application);
+
+        // log
+        // 此 log 將自動記錄到 CATALINA_HOME/logs 內
+        // application.log("大家好，我是2023最悲慘的康芝魚");
+
+        int age = 17;
+        // 當年紀小於18，表示非法並記入日誌
+        if(age < 18) {
+            application.log("很抱歉，您未成年！", new RuntimeException("請走開"));
+        }
+
+
     }
 }
