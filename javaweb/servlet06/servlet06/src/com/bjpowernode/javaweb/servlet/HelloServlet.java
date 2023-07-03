9a04ed2f0c0a3d81264a3ac0bed89df93bff4490
Maggie Kang <maggiekang86@gmail.com>
2023-07-03 16:50:38 +0800

    19-37:51

--- javaweb/servlet06/servlet06/src/com/bjpowernode/javaweb/servlet/HelloServlet.java
+++ javaweb/servlet06/servlet06/src/com/bjpowernode/javaweb/servlet/HelloServlet.java
@@ -1,6 +1,11 @@
 package com.bjpowernode.javaweb.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     // 通過無參數構造方法創建對象
@@ -13,5 +18,12 @@ public class HelloServlet extends HttpServlet {
     // HttpServlet 類中沒有 init 方法，會繼續執行 GenericServlet 類中的 init 方法
 
     // 沒有提供 service 方法。 那麼必然執行父類 HttpServlet 類 service 方法
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+    }
+
 
 }
