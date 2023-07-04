43e155763e56218289eb7b782e119286f4bdafa8
Maggie Kang <maggiekang86@gmail.com>
2023-07-04 16:33:28 +0800

    結束19

--- javaweb/servlet06/servlet06/src/com/bjpowernode/javaweb/servlet/HelloServlet.java
+++ javaweb/servlet06/servlet06/src/com/bjpowernode/javaweb/servlet/HelloServlet.java
@@ -18,12 +18,23 @@ public class HelloServlet extends HttpServlet {
     // HttpServlet 類中沒有 init 方法，會繼續執行 GenericServlet 類中的 init 方法
 
     // 沒有提供 service 方法。 那麼必然執行父類 HttpServlet 類 service 方法
-    @Override
+
+    /*@Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.print("<h1>Hello Servlet</h1>");
-    }
+    }*/
 
+    // 當前端發送的請求是get請求的時候，這裡重寫doGet方法
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<h1>doGet</h1>");
+    }
+    // 當前端發送的請求是post請求的時候，這裡重寫doPost方法
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<h1>doPost</h1>");
+    }
 
 }
